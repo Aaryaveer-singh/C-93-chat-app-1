8b1d32b203e7d38e0a552be2df7435b540c780bc
Aaryaveer-singh <86355394+Aaryaveer-singh@users.noreply.github.com>
2022-02-05 11:40:08 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,22 @@
+var firebaseConfig = {
+      apiKey: "AIzaSyBP5URB16GZIal8gpbKD2Q5mjPv82CyDqI",
+      authDomain: "kwitter-3a98e.firebaseapp.com",
+      databaseURL: "https://kwitter-3a98e-default-rtdb.firebaseio.com",
+      projectId: "kwitter-3a98e",
+      storageBucket: "kwitter-3a98e.appspot.com",
+      messagingSenderId: "207609264061",
+      appId: "1:207609264061:web:a5a3aec33edc347008c4d4"
+    };
+    
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
+user_name=localStorage.getItem("user_name_key");
+document.getElementById("user_name").innerHTML="welcome "+ user_name;
